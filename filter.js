bca9b292b4cc93a2cb3d09026bca9734488d37b6
Nikita Ershov <67161360+Nikshov@users.noreply.github.com>
2022-06-01 21:46:47 +0300

    Create filter.js

---
+++ filter.js
@@ -0,0 +1,10 @@
+function fltrd(arr, range) {
+    function filt(cours) {
+        if (cours.prices[0] <= range[1] || range[1] === null) {
+            if (cours.prices[1] >= range[0] || cours.prices[1] === null)
+                return true
+        }
+        return false
+    }
+    return arr.filter(filt);
+}
