d97530508aa4c1ae865e6ae017dad230689a10d9
Nikita Ershov <67161360+Nikshov@users.noreply.github.com>
2022-06-01 21:47:13 +0300

    Create sort.js

---
+++ sort.js
@@ -0,0 +1,11 @@
+function sortd(arr) {
+  function compare(a, b) {
+  if (a.prices[0] < b.prices[0]) return -1;
+  if (a.prices[0] > b.prices[0]) return 1;
+  if (a.prices[0] == b.prices[0]) {
+    if (a.prices[1] < b.prices[1]) return -1;
+    else return 1;
+  }
+  }
+  return arr.sort(compare);
+}
